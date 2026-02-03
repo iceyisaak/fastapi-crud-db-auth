0134228c338b72a0cf1e563a54958d6330333a45
iceyisaak <me@iceyisaak.com>
2026-02-03 14:27:13 +0100

    Moved app.py to __init__.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 if __name__=="__main__":
     uvicorn.run(
-        app="app:app",
+        app="app.__init__:app",
         host="127.0.0.1",
         port=8000,
         reload=True,
