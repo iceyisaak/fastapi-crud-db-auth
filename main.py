f3730d557313cefa00355c8852647f7d89d6e92a
iceyisaak <me@iceyisaak.com>
2026-02-06 09:05:25 +0100

    Refactored project

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import uvicorn
+def main():
+    print("Hello from fastapi-crud-db-auth!")
 
-if __name__=="__main__":
-    uvicorn.run(
-        app="app.__init__:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True,
-    )
+
+if __name__ == "__main__":
+    main()
