57adbb208665a2fc6f70f5c5f870a9771633c118
iceyisaak <me@iceyisaak.com>
2026-02-11 15:54:45 +0100

    Added email.py

---
+++ src/middlewares.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+
+class middleware:
+    def __init__(self, app):
+        pass
+
+
+    @staticmethod
+    async def register_middleware(app:FastAPI):
+        pass
+
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins: ["*"],
+#     allow_methods=[""],
+#     allow_headers=[""],
+#     allow_credentials=True
+# )
+
